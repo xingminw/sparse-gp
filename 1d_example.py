c04ad2f67f6888e48efd20d80072d7fdc1bf54d2
Jacob Charles Saldinger <jsald@umich.edu>
2023-04-05 13:01:07 -0400

    Add additional comparison metrics and heuristical sparse GP

--- 1d_example.py
+++ 1d_example.py
@@ -5,11 +5,12 @@ This code is modified based on
 
 import numpy as np
 import matplotlib.pyplot as plt
-from gp_algs import SparseVariationalGP, ExactGP
-
+from gp_algs import SparseVariationalGP, ExactGP, kmpp_gp
+from sklearn.metrics import mean_squared_error
+from time import time
 
 # Make some 1D training data
-total_num = 500  # 500 training points
+total_num = 2000  # 500 training points
 x_range = 10
 # raw_points_x = np.linspace(0, x_range, total_num)    # Inputs evenly spaced between 0 and 10
 raw_points_x = np.random.rand(total_num) * x_range    # Inputs randomly spaced between 0 and 100
@@ -27,10 +28,10 @@ predict_y_mean = np.zeros(predict_num)     # Initialise GP mean predictions
 predict_y_std = np.zeros(predict_num)      # Initialise GP std predictions
 
 # Train sparse GP
-inducing_num = 8                  # No. sparse points
+inducing_num = 8                # No. sparse points
 candidate_num = 100      # No. of candidate sets of sparse points analysed
 
-sgp, gp = True, True
+sgp, gp, kmpp = True, True, True
 
 # Initialize Sparse GP regression
 if sgp:
@@ -53,6 +54,12 @@ if sgp:
     for n in range(predict_num):
         local_x = predict_x[n]
         predict_y_mean[n], predict_y_std[n] = sgp.predict(local_x)
+
+    t0 = time()
+    y_pred = np.array([sgp.predict(x_)[0][0] for x_ in raw_points_x])
+    t1 = time()
+    print('RMSE:', np.round(mean_squared_error(raw_points_y, y_pred, squared=False), 3))
+    print('Prediction time:', np.round(t1 - t0, 3))
 
     # Plot results
     plt.figure()
@@ -86,6 +93,13 @@ if gp:
         local_x = predict_x[n]
         predict_y_mean[n], predict_y_std[n] = gp.predict(local_x)
 
+    t0 = time()
+    y_pred = np.array([gp.predict(x_)[0][0] for x_ in raw_points_x])
+    t1 = time()
+    print('RMSE:', np.round(mean_squared_error(raw_points_y, y_pred, squared=False), 3))
+    print('Prediction time:', np.round(t1 - t0, 3))
+
+
     # Plot results
     plt.figure()
     plt.grid()
@@ -95,5 +109,47 @@ if gp:
     plt.plot(predict_x, predict_y_mean - 3 * predict_y_std, 'black')
     plt.xlabel('x')
     plt.title("Exact GP")
+    plt.legend()
+    plt.show()
+
+if kmpp:
+    print("##################### kmeans++ heuristical sparse GP #####################")
+    kmppgp = kmpp_gp.SparseKmppGP()
+
+    # Load data to GP regression
+    kmppgp.load_data(raw_points_x, raw_points_y, total_num)
+
+    # Initialize the trainer and train the GP
+    kmppgp.init_trainer(initial_l, initial_sigma, inducing_num)
+    elapsed_time = kmppgp.train(candidate_num)
+    # lb_best, elapsed_time = sgp.train_greedy(candidate_num)
+
+    # Print results
+    print('Hyperparameters:', np.round(kmppgp.width_l, 3), np.round(kmppgp.sigma, 3))
+    print('Elapsed time:', np.round(elapsed_time, 3))
+
+    for n in range(predict_num):
+        local_x = predict_x[n]
+        predict_y_mean[n], predict_y_std[n] = kmppgp.predict(local_x)
+
+    t0 = time()
+    y_pred = np.array([kmppgp.predict(x_)[0][0] for x_ in raw_points_x])
+    t1 = time()
+    print('RMSE:', np.round(mean_squared_error(raw_points_y, y_pred, squared=False), 3))
+    print('Prediction time:', np.round(t1 - t0, 3))
+
+    # Plot results
+    plt.figure()
+    plt.grid()
+    plt.plot(raw_points_x, raw_points_y, '.', color='blue', label='Full dataset')
+    plt.plot(kmppgp.inducing_points_x, kmppgp.inducing_points_y, 'o',
+             markeredgecolor='black', markerfacecolor='red',
+             markeredgewidth=1.5, markersize=10, label='Sparse dataset')
+    plt.plot(predict_x, predict_y_mean, 'black', label='Sparse GP')
+    plt.plot(predict_x, predict_y_mean + 3 * predict_y_std, 'black')
+    plt.plot(predict_x, predict_y_mean - 3 * predict_y_std, 'black')
+
+    plt.xlabel('x')
+    plt.title("Sparse kmeans++ GP")
     plt.legend()
     plt.show()
--- gp_algs/kmpp_gp.py
+++ gp_algs/kmpp_gp.py
@@ -9,9 +9,11 @@ import numpy as np
 from scipy.optimize import minimize
 from .gp_base import GaussianProcesses
 from tqdm import tqdm
+import scipy.sparse as sp
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.utils.extmath import stable_cumsum, row_norms
 
-
-class SparseVariationalGP(GaussianProcesses):
+class SparseKmppGP(GaussianProcesses):
     def __init__(self):
         # input params
         super().__init__()
@@ -29,32 +31,113 @@ class SparseVariationalGP(GaussianProcesses):
         self.sigma = initial_sigma
         self.inducing_num = inducing_num
 
+    def k_meansplusplus(self, X, Y, n_clusters, random_state=np.random.RandomState,
+                        n_local_trials=None):  # Copied from sklearn library
+        """Init n_clusters seeds according to k-means++, adapted from sklearn
+        Parameters
+        ----------
+        X : array or sparse matrix, shape (n_samples, n_features)
+            The data to pick seeds for. To avoid memory copy, the input data
+            should be double precision (dtype=np.float64).
+        n_clusters : integer
+            The number of seeds to choose
+        random_state : int, RandomState instance
+            The generator used to initialize the centers. Use an int to make the
+            randomness deterministic.
+            See :term:`Glossary <random_state>`.
+        n_local_trials : integer, optional
+            The number of seeding trials for each center (except the first),
+            of which the one reducing inertia the most is greedily chosen.
+            Set to None to make the number of trials depend logarithmically
+            on the number of seeds (2+log(k)); this is the default.
+        Notes
+        -----
+        Selects initial cluster centers for k-mean clustering in a smart way
+        to speed up convergence. see: Arthur, D. and Vassilvitskii, S.
+        "k-means++: the advantages of careful seeding". ACM-SIAM symposium
+        on Discrete algorithms. 2007
+        Version ported from http://www.stanford.edu/~darthur/kMeansppTest.zip,
+        which is the implementation used in the aforementioned paper.
+        """
+        # Calculated w/o initialization speed up trick in sklearn
+        x_squared_norms = row_norms(X, squared=True)
+        n_samples, n_features = X.shape
+
+        centers = np.empty((n_clusters, n_features), dtype=X.dtype)
+        centersy = np.empty((n_clusters, n_features), dtype=X.dtype)
+
+        assert x_squared_norms is not None, 'x_squared_norms None in _k_init'
+
+        # Set the number of local seeding trials if none is given
+        if n_local_trials is None:
+            # This is what Arthur/Vassilvitskii tried, but did not report
+            # specific results for other than mentioning in the conclusion
+            # that it helped.
+            n_local_trials = 2 + int(np.log(n_clusters))
+
+        # Pick first center randomly
+        center_id = np.random.randint(n_samples)
+        if sp.issparse(X):
+            centers[0] = X[center_id].toarray()
+            centersy[0] = Y[center_id].toarray()
+
+        else:
+            centers[0] = X[center_id]
+            centersy[0] = Y[center_id]
+
+
+        # Initialize list of closest distances and calculate current potential
+        closest_dist_sq = euclidean_distances(
+            centers[0, np.newaxis], X, Y_norm_squared=x_squared_norms,
+            squared=True)
+        current_pot = closest_dist_sq.sum()
+
+        # Pick the remaining n_clusters-1 points
+        for c in range(1, n_clusters):
+            # Choose center candidates by sampling with probability proportional
+            # to the squared distance to the closest existing center
+            rand_vals = np.random.random_sample(n_local_trials) * current_pot
+            candidate_ids = np.searchsorted(stable_cumsum(closest_dist_sq),
+                                            rand_vals)
+            # XXX: numerical imprecision can result in a candidate_id out of range
+            np.clip(candidate_ids, None, closest_dist_sq.size - 1,
+                    out=candidate_ids)
+
+            # Compute distances to center candidates
+            distance_to_candidates = euclidean_distances(
+                X[candidate_ids], X, Y_norm_squared=x_squared_norms, squared=True)
+
+            # update closest distances squared and potential for each candidate
+            np.minimum(closest_dist_sq, distance_to_candidates,
+                       out=distance_to_candidates)
+            candidates_pot = distance_to_candidates.sum(axis=1)
+
+            # Decide which candidate is the best
+            best_candidate = np.argmin(candidates_pot)
+            current_pot = candidates_pot[best_candidate]
+            closest_dist_sq = distance_to_candidates[best_candidate]
+            best_candidate = candidate_ids[best_candidate]
+
+            # Permanently add best center candidate found in local tries
+            if sp.issparse(X):
+                centers[c] = X[best_candidate].toarray()
+                centersy[c] = Y[best_candidate].toarray()
+
+            else:
+                centers[c] = X[best_candidate]
+                centersy[c] = Y[best_candidate]
+
+
+        return centers, centersy
+
     def train(self, candidate_num):
         start_time = time.time()                    # Note the time when training began
         theta = np.array([self.width_l, self.sigma])  # GP hyperparameters
 
         # Loop over all the candidate sparse points
-        new_selected_x, new_selected_y, lb_best = None, None, None
-        for r in range(candidate_num):
+        new_selected_x, new_selected_y = self.k_meansplusplus(self.points_x, self.points_y, n_clusters=self.inducing_num)
 
-            # Randomly select the locations of the candidate sparse points
-            indices = random.sample(range(self.points_num), self.inducing_num)
-            candidate_x = self.points_x[indices]
 
-            # Define arguments to be passed to 'NegLowerBound' and
-            # evaluate the lower bound for candidate sparse points
-            a = candidate_x
-            lb = - self._get_neg_lower_bound(theta, a)
-
-            if r == 0:  # For the first set of candidate points
-                new_selected_x = candidate_x  # Define optimum sparse inputs found so far
-                new_selected_y = self.points_y[indices]  # Define optimum sparse outputs found so far
-                lb_best = lb  # Store maximum lower bound found so far
-            else:
-                if lb > lb_best:  # If lower bound is largest we've seen
-                    new_selected_x = candidate_x  # Define optimum sparse inputs found so far
-                    new_selected_y = self.points_y[indices]  # Define optimum sparse outputs found so far
-                    lb_best = lb  # Store maximum lower bound found so far
         # Update hyperparameters using scipy.minimize
         # Arguments needed as input to 'minimize' function
         a = new_selected_x
@@ -73,7 +156,7 @@ class SparseVariationalGP(GaussianProcesses):
         self.width_l, self.sigma = theta  # Extract hyperparameters
         self.update_matrices()
         elapsed_time = time.time() - start_time     # Time taken for training
-        return lb_best, elapsed_time
+        return elapsed_time
 
     def train_greedy(self, candidate_num):
         start_time = time.time()  # Note the time when training began
